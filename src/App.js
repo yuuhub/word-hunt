5098e625621d25227fbe4ccc0977f4b2d1f0461f
yuuhub <71030036+yuuhub@users.noreply.github.com>
2022-04-27 14:33:53 +0800

    Add Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from "axios";
+import { Container } from '@mui/material';
+import Header from './components/Header';
 
 function App() {
+
+  const [word, setWord] = useState("");
+  const [meanings, setMeanings] = useState([]);
+
+  const dictionaryApi = async() => {
+    try {
+      const data = await axios.get('https://api.dictionaryapi.dev/api/v2/entries/en/plane');
+      setMeanings(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  console.log(meanings);
+
+  useEffect(() => {
+    dictionaryApi();
+  }, [])
+
   return (
-    <div className="App">
-      Hello World
+    <div className="App" style={{height: "100vh", backgroundColor: "#282c34", color: "white"}}>
+      <Container 
+        maxWidth="md" 
+        style={{display: "flex", flexDirection: "column", height: "100vh"}}
+      >
+        <Header />
+      </Container>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+        <span className="title">Word Hunt</span>
+        <div className='inputs'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Header
